579df4a2ea665de2de23adba09476f5508c6c076
Israel Paul <130955174+I-paul@users.noreply.github.com>
2025-03-22 10:17:14 +0530

    not much here

--- secure-ai/src/firebase.js
+++ secure-ai/src/firebase.js
@@ -4,13 +4,13 @@ import { getAuth } from "firebase/auth";
 
 // Your Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDy_k0ZBIHVtFuKegm8SmzIA7UMQ3ibADI",
-  authDomain: "ai-72f78.firebaseapp.com",
-  projectId: "ai-72f78",
-  storageBucket: "ai-72f78.firebasestorage.app",
-  messagingSenderId: "72552640869",
-  appId: "1:72552640869:web:3e3b3d3cec88d552b7318c",
-  measurementId: "G-M3KRTTPBNV"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.measurementId
 };
 
 // Initialize Firebase only once
